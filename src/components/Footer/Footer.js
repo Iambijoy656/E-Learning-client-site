eb19d68a2b5ff81d9542d0ef742723e570db16f8
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-10-25 10:04:13 +0600

    home page complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import img from '../../assets/download.png';
 const Footer = () => {
     return (
         <div>
-            <footer className="px-4 divide-y bg-gray-100 dark:text-gray-800">
+            <footer className="px-4 divide-y bg-gray-100 dark:text-gray-800 mt-5">
                 <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
                     <div className="lg:w-1/3">
                         <img src={img} alt="" />
