703c9cde404effe8b84929c5f4313fe435f08e9a
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-10-27 18:24:37 +0600

    checkout page update

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -17,7 +17,10 @@ const Checkout = () => {
             <h2 className='text-2xl text-violet-900 font-medium text-center my-5'>CheckOut Page</h2>
 
             <div className='text-xl mb-3'>
-                <h4 >User Name: {user?.displayName}</h4>
+                <h1 className='text-center font-bold'>
+                    Welcome <span className="text-violet-700">{user?.displayName}</span>, to the
+                    checkout page.
+                </h1>
                 <h2>Email: {user.email}</h2>
             </div>
             <button onClick={handleCheckOut} className="btn btn-outline btn-warning">Confirm CheckOut</button>
